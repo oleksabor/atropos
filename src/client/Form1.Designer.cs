d2a53e7f27ff15f7ec5c0447da00add498a00ed3
oleksa borodie <oleksa.borodie@gmail.com>
2019-02-06 12:41:44 +0200

    grid and binding sources #1

--- src/client/Form1.Designer.cs
+++ src/client/Form1.Designer.cs
@@ -30,13 +30,17 @@
 		{
 			this.components = new System.ComponentModel.Container();
 			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.lUser = new System.Windows.Forms.ToolStripLabel();
 			this.cbUsers = new System.Windows.Forms.ToolStripComboBox();
-			this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.bsUser = new System.Windows.Forms.BindingSource(this.components);
+			this.bsCurfew = new System.Windows.Forms.BindingSource(this.components);
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
 			this.toolStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.bsUser)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.bsCurfew)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// toolStrip1
@@ -50,15 +54,6 @@
 			this.toolStrip1.TabIndex = 0;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dataGridView1.Location = new System.Drawing.Point(0, 25);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(800, 425);
-			this.dataGridView1.TabIndex = 1;
-			// 
 			// lUser
 			// 
 			this.lUser.Name = "lUser";
@@ -70,11 +65,31 @@
 			this.cbUsers.Name = "cbUsers";
 			this.cbUsers.Size = new System.Drawing.Size(121, 25);
 			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(800, 425);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AutoGenerateColumns = false;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.DataSource = this.bsCurfew;
+			this.dataGridView2.Location = new System.Drawing.Point(8, 33);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+			this.dataGridView2.TabIndex = 2;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView2);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.toolStrip1);
 			this.Name = "Form1";
@@ -82,7 +97,9 @@
 			this.toolStrip1.ResumeLayout(false);
 			this.toolStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.bsUser)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.bsCurfew)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -94,7 +111,9 @@
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.ToolStripLabel lUser;
 		private System.Windows.Forms.ToolStripComboBox cbUsers;
-		private System.Windows.Forms.BindingSource bindingSource1;
+		private System.Windows.Forms.BindingSource bsUser;
+		private System.Windows.Forms.BindingSource bsCurfew;
+		private System.Windows.Forms.DataGridView dataGridView2;
 	}
 }
 
