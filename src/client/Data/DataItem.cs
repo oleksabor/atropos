fc3cc6964ce78373dc3e3fdaf4ab71946fb0a46e
oleksa borodie <oleksa.borodie@gmail.com>
2019-03-07 16:46:45 +0200

    Finished is shown in GUI #14

--- src/client/Data/DataItem.cs
+++ src/client/Data/DataItem.cs
@@ -1,14 +1,16 @@
 ﻿using Atropos.Common.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace client.Data
 {
-	public class DataItem<T> : PropertyChangedBase
+	public class DataItem<T> : PropertyChangedBase, INotifyDataErrorInfo
 	{
 		static ILog Log = LogProvider.GetCurrentClassLogger();
 
@@ -28,22 +30,40 @@ namespace client.Data
 		{
 			try
 			{
+				errors.Clear(nameof(Value));
 				Value = LoadData();
 			}
 			catch (Exception e)
 			{
 				var message = string.Format("failed to load data:{0}", typeof(T));
 				Log.ErrorException(message, e);
+				errors.Add(e.Message, this, nameof(Value));
 				throw new ApplicationException(message, e);
 			}
 		}
 
+		ErrorsChanger errors = new ErrorsChanger();
+
+		public IEnumerable GetErrors(string propertyName)
+		{
+			return errors.GetErrors(propertyName);
+		}
+
 		private T _v;
+
+		public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged
+		{
+			add { errors.ErrorsChanged += value; }
+			remove { errors.ErrorsChanged -= value; }
+		}
+
 		public T Value
 		{
 			get { return _v; }
 			set { Set(ref _v, value); }
 		}
+
+		public bool HasErrors => errors.HasErrors;
 	}
 
 	public class DataItems<TItem> : DataItem<ObservableCollection<TItem>>
